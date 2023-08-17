006a176c3daef6f763a0e18f06e9af38739c1193
Dimitri-Igoshev <d.o.igoshev@gmail.com>
2023-08-17 12:15:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-screen h-screen flex justify-center items-center text-5xl text-white font-bold">
+    <div className="p-5 w-screen h-screen flex justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-2xl text-white font-bold">
       Website coming soon!
     </div>
   );
